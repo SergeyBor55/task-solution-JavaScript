96f07abcb04ff89519f84c5d3c79d3b9f4b3908d
SergeyBor55 <43942010+SergeyBor55@users.noreply.github.com>
2019-02-17 12:32:14 +0300

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1,18 +1,6 @@
 /**
  * Created by сергей on 03.07.2018.
  */
-
-<script>
-//Конвертер валют
-var a = +prompt("Введите сумму в рублях");
-var b = 62;
-var c = 70;
-var dollar = (a / b).toFixed(2);
-var euro = (a / c).toFixed(2);
-alert(dollar + " в $ и \n" + euro + " в евро");
-</script>
-
-
 <script>
 //Игра в загадки
 var a = 0;
@@ -36,6 +24,23 @@ if ('лампочка' == answer2) {
 
 
 <script>
+/*Создайте функцию filterRange(arr, a, b), которая принимает массив чисел arr и возвращает новый массив,
+который содержит только числа из arr из диапазона от a до b.*/
+var arr = [5, 4, 3, 8, 0];
+
+function filterRange(arr, a, b) {
+    var arr1 = [];
+    for(var i =0; i <= arr.length; i++) {
+        if(a <= arr[i] && arr[i] <= b)  arr1.push(arr[i]);
+    }
+    return arr1;
+}
+var filtered = filterRange(arr, 3, 5);
+alert( filtered );
+</script>
+
+
+<script>
 //Расчёт % от вклада в банк
 var contribution = +prompt('Введите сумму вклада');
 var percent = +prompt('Введите процент');
@@ -45,6 +50,28 @@ while (i <= 5) {
     alert('Ваша прибыль за ' + i + ' год ' + contribution.toFixed(2))
     i++
 }
+</script>
+
+
+<script>
+ /*Есть объект salaries с зарплатами. Напишите код, который выведет имя сотрудника, у которого самая большая зарплата.
+ "use strict";*/
+
+ var salaries = {
+ "Вася": 100,
+ "Петя": 300,
+ "Даша": 250
+ };
+
+ var maxSalary = 0;
+ var name = '';
+ for (var key in salaries) {
+ if (maxSalary < salaries[key]) {
+ maxSalary = salaries[key];
+ name = key;
+ }
+ }
+ alert(name || 'нет сотрудников');
 </script>
 
 
@@ -71,4 +98,30 @@ while (true) {
         playerNum = 2;
     }
 }
+</script>
+
+
+<script>
+//Cоздайте функцию isEmpty(obj), которая возвращает true, если в объекте нет свойств и false – если хоть одно свойство есть.
+ function isEmpty(obj) {
+ var count = 0;
+ for(key in schedule) {
+ count++;
+ }
+ return count ? false : true;
+ }
+ var schedule = {};
+ schedule.name = 'vity';
+ alert( isEmpty(schedule) );
+</script>
+
+
+<script>
+//Конвертер валют
+var a = +prompt("Введите сумму в рублях");
+var b = 62;
+var c = 70;
+var dollar = (a / b).toFixed(2);
+var euro = (a / c).toFixed(2);
+alert(dollar + " в $ и \n" + euro + " в евро");
 </script>
