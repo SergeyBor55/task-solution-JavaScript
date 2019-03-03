67a5fa027a489f0d3eb92e465816f718c564abd8
SergeyBor55 <43942010+SergeyBor55@users.noreply.github.com>
2019-03-03 18:19:54 +0300

    Update solutions.js

--- solutions.js
+++ solutions.js
@@ -1,7 +1,73 @@
 /**
  * Created by сергей on 03.07.2018.
  */
-<script>
+/*Объект salaries с зп сотрудников.
+1. Напишите функцию проверки на пустоту объекта.
+2. Напишите код, который выведет сумму всех зарплат, если объект пустой, то результат должен быть 0.
+3. Напишите код, который выведет имя сотрудника с максимальной зп, если объект пуст то пусть выведет «нет сотрудников»
+4. Еапишите функцию которая будет добавлять сумму премии к сумме зп сотрудника по его имени в объекте*/
+
+var salaries = {
+  "Игорь": 40000,
+  "Мария": 35000,
+  "Дмитрий": 55000 
+};
+
+function isEmptyObj(obj) {
+  for (var key in obj){
+      return false;
+  }
+  return true;
+}
+
+function sumSalary(obj) {
+  var sum = 0;
+  for (var key in obj){
+      sum += obj[key];
+  }
+  return sum;
+}
+
+function maxSalary(obj) {
+	if(isEmpty(obj)) {
+		return 'Нет сотрудников';
+	} else {
+		var maxSalary = 0;
+		var name = '';
+		for(var key in obj) {
+			if(maxSalary < obj[key]) {
+				maxSalary = obj[key];
+				name = key;
+			}
+		}
+	}
+	return name;
+}
+
+function addTheAwardToTheEmployee(obj, name, sum) {
+	
+	if(!name || typeof name !== "string") { 
+	    return 'Функция addTheAwardToTheEmployee ожидает вторым параметром строку с "именем" значение';
+	}
+    else if(!sum || typeof sum !== "number") { 
+	    return 'Функция addTheAwardToTheEmployee ожидает третим параметром числовое значение суммы';
+	}
+    else if(typeof obj !== "object" || isEmptyObj(obj)) {
+		return 'Функция addTheAwardToTheEmployee ожидает первым параметром объект и непустой';
+    }
+	
+	for (var key in obj){
+      if(name == key) {
+		 obj[key] += sum;
+		 return obj[key];
+	  }   
+  }
+  return 'Нет сотрудника с таким именем';	
+}
+//alert(addTheAwardToTheEmployee(salaries, 'Дмитрий', 3000));
+
+
+
 //Игра в загадки
 var a = 0;
 alert('Пивет! Давай поиграем в загадки');
@@ -20,10 +86,9 @@ if ('лампочка' == answer2) {
 } else {
     alert('Неверно! Игра окончена. Правильных ответов: ' + a);
 }
-</script>
 
 
-<script>
+
 /*Создайте функцию filterRange(arr, a, b), которая принимает массив чисел arr и возвращает новый массив,
 который содержит только числа из arr из диапазона от a до b.*/
 var arr = [5, 4, 3, 8, 0];
@@ -37,10 +102,9 @@ function filterRange(arr, a, b) {
 }
 var filtered = filterRange(arr, 3, 5);
 alert( filtered );
-</script>
 
 
-<script>
+
 //Расчёт % от вклада в банк
 var contribution = +prompt('Введите сумму вклада');
 var percent = +prompt('Введите процент');
@@ -50,10 +114,9 @@ while (i <= 5) {
     alert('Ваша прибыль за ' + i + ' год ' + contribution.toFixed(2))
     i++
 }
-</script>
 
 
-<script>
+
  /*Есть объект salaries с зарплатами. Напишите код, который выведет имя сотрудника, у которого самая большая зарплата.
  "use strict";*/
 
@@ -72,10 +135,9 @@ while (i <= 5) {
  }
  }
  alert(name || 'нет сотрудников');
-</script>
 
 
-<script>
+
 //Угадайте число
 var answer = parseInt(Math.random() * 100);
 var playerNum = 1;
@@ -98,10 +160,9 @@ while (true) {
         playerNum = 2;
     }
 }
-</script>
 
 
-<script>
+
 //Cоздайте функцию isEmpty(obj), которая возвращает true, если в объекте нет свойств и false – если хоть одно свойство есть.
  function isEmpty(obj) {
  var count = 0;
@@ -113,10 +174,9 @@ while (true) {
  var schedule = {};
  schedule.name = 'vity';
  alert( isEmpty(schedule) );
-</script>
 
 
-<script>
+
 //Конвертер валют
 var a = +prompt("Введите сумму в рублях");
 var b = 62;
@@ -124,4 +184,3 @@ var c = 70;
 var dollar = (a / b).toFixed(2);
 var euro = (a / c).toFixed(2);
 alert(dollar + " в $ и \n" + euro + " в евро");
-</script>
